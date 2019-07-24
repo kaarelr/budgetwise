3be965a8e09837ccb6ce318cba3e4ba677fb750d
Kaarel Reinvars <kaarel.reinvars@gmail.com>
2019-07-24 22:44:14 +0300

    Changed git url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='budgetwise',
       version='0.1',
       description='Easily calculate budgets for your company',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/kaarelr/budgetwise',
       author='Cloudberry',
       author_email='kaarel.reinvars@gmail.com',
       license='MIT',
