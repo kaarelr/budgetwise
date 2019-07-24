5d7ed7084d57da71300374d47370cadcc8728560
Kaarel Reinvars <kaarel.reinvars@gmail.com>
2019-07-24 22:41:23 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='budgetwise',
+      version='0.1',
+      description='Easily calculate budgets for your company',
+      url='http://github.com/storborg/funniest',
+      author='Cloudberry',
+      author_email='kaarel.reinvars@gmail.com',
+      license='MIT',
+      packages=['budgetwise'],
+      zip_safe=False)
